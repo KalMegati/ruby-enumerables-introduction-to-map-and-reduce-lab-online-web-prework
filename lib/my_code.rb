a231db165d818d234e134e279bb184201b52536a
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 01:20:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,11 @@ end
 
 
 def map_to_no_change(source_array)
-  source_array
+  new_array = []
+  source_array.length.times do |index|
+    new_array.push(source_array[index])
+  end
+  new_array
 end
 
 

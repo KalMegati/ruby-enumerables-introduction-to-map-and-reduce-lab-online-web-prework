e2ad1f8cf6baaacba1011d563ace1eb0bcf24f2f
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 01:33:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,7 @@ end
 def map_to_double(source_array)
   counter = 0
   while counter < source_array.length do
-    source_array[counter] *= 2
+    source_array[counter] = source_array[counter]*2
     counter += 1
   end
   source_array
